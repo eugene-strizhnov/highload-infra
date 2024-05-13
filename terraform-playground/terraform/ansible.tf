c19dbe3d0bea14b8eb7355a4968235cdb43a4dac
Eugene Strizhnov <ystrizhnov@gmail.com>
2024-05-12 20:38:17 -0700

    fix ansible execution

--- terraform-playground/terraform/ansible.tf
+++ terraform-playground/terraform/ansible.tf
@@ -3,10 +3,13 @@ locals {
 }
 
 resource "terraform_data" "ansible" {
-  # Changes to any instance of the cluster requires re-provisioning
-  triggers_replace = [
-    yandex_compute_instance.nginx_vm.id
-  ]
+#   Changes to any instance of the cluster requires re-provisioning
+#   triggers_replace = [
+#     yandex_compute_instance.nginx_vm.id
+#   ]
+
+#   re-provision every time
+  triggers_replace = timestamp()
 
   provisioner "remote-exec" {
     inline = ["sudo echo 'Hello'"]
@@ -14,15 +17,14 @@ resource "terraform_data" "ansible" {
     connection {
       type        = "ssh"
       user        = local.user_name
-#      private_key = file(var.private_ssh_key_path)
       host        = yandex_compute_instance.nginx_vm.network_interface.0.nat_ip_address
     }
   }
 
   provisioner "local-exec" {
     command = <<-EOF
-      ansible-playbook -u ${local.user_name} \
-      -i ${yandex_compute_instance.nginx_vm.network_interface.0.nat_ip_address} ${local.ansible_path}
+      ansible-playbook -u ${local.user_name} --ssh-common-args='-o StrictHostKeyChecking=accept-new' \
+      -i '${yandex_compute_instance.nginx_vm.network_interface.0.nat_ip_address},' ${local.ansible_path}
     EOF
   }
 }
